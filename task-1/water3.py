5248a0008c8b1d2f3b1098e84d055835a5e35101
pololun <79504369+pololun@users.noreply.github.com>
2023-09-11 15:38:06 +0700

    Update water3.py
    Done

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,33 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0] == 4 and s[1]==4
 
 def successors(s):
     x, y, z = s
-    return []
+    
+
+    if x>0 : #8--->5
+        if y<5:
+            min_var = min(x,5-y)
+            yield(((x-min_var,y+min_var,z),min_var))
+        if z<3:
+            min_var = min(x,3-z)
+            yield(((x-min_var,y,z+min_var),min_var))
+    if y>0:
+        if x<8:
+            min_var =min(y,8-x)
+            yield(((x+min_var,y-min_var,z),min_var))
+        if z<3:
+            min_var =min(y,3-z)
+            yield(((x,y-min_var,z+min_var),min_var))
+    if z>0:
+        if x<8:
+            min_var = min(z,8-x)
+            yield(((x+min_var,y,z-min_var),min_var))
+        if y<5:
+            min_var = min(z,5-y)
+            yield(((x,y+min_var,z-min_var),min_var))
+
+    
